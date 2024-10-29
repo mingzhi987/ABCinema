e5b38c10484940970420177c78917f42474419ab
mingzhi987 <106290967+mingzhi987@users.noreply.github.com>
2024-10-29 11:22:40 +0800

    updated header and footer

---
+++ checkout.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="abcmovies.css">
+    <title>Movies | ABCinema</title>
+</head>
+
+<body>
+    <!-- Nav bar -->
+    <div class="header">
+        <a href="#default">
+            <img class=logo src="images/logo/logo.png" href="#">
+        </a>
+        <div class="header-left">
+            <a href="#contact" alt="Contact">Contact</a>
+            <a href="#about" alt="About">About us</a>
+        </div>
+        <div class="menu-icons">
+            <a href="#"><img src="images/icons/basket.svg" alt="Checkout" /></a>
+            <a href="#"><img src="images/icons/profile.svg" alt="Profile" /></a>
+        </div>
+    </div>
+
+
+    <!-- Footer -->
+    <footer>
+        <div class="footer-container">
+            <div class="row">
+                <div class="column-1"><img class="logo" src="images/logo/logo.png">
+                    <div class="footer-summary">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</div>
+                </div>
+
+                <div class="column-2">
+                    <h4>Links</h4>
+                    <ul>
+                        <li class="footer-links"><a href="http://demo.amytheme.com/movie/demo/elementor-single-cinema/" aria-current="page">Home</a></li>
+                        <li class="footer-links"><a href="http://demo.amytheme.com/movie/demo/elementor-single-cinema/coming-soon/">Coming Soon</a></li>
+                        <li class="footer-links"><a href="http://demo.amytheme.com/movie/demo/elementor-single-cinema/top-rated/">Top rated</a></li>
+                    </ul>
+                </div>
+
+                <div class="column-2">
+                    <h4>Contact Us</h4>
+                    123 Raffles Place #14-01 <br> Singapore 348023
+                    <p>support@abcinema.com.sg
+                    <p>+65 63498203
+                </div>
+
+                <div class="column-2">
+                    <h4>Follow Us</h4>
+                    <img src="images/icons/twitter-x.svg" alt="X (Twitter)" />
+                    <img src="images/icons/facebook.svg" alt="Facebook" />
+                    <img src="images/icons/instagram.svg" alt="Instagram" />
+                </div>
+            </div>
+        </div>
+        </div>
+    </footer>
+
+</html>
